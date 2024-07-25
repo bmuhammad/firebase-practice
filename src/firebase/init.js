d0927bb457a7a01639365abd31e9455cd4bd87c2
Brian <briancmuhammad@yahoo.com>
2024-07-25 13:32:36 -0400

    initialized firebase authentication Repo

---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDioWtBQtVOwg2wdy5irJSIGAL5B1EOwH4",
+  authDomain: "fir-practice-de528.firebaseapp.com",
+  projectId: "fir-practice-de528",
+  storageBucket: "fir-practice-de528.appspot.com",
+  messagingSenderId: "72330496950",
+  appId: "1:72330496950:web:f9f86acd2be5f02a3d8f17"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
